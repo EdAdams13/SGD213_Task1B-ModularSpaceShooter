e644e04e14e82cf9d679e2caf587640a407e8c5d
EdAdams12 <E_A055@student.usc.edu.au>
2024-03-29 16:37:27 +1000

    Inital Push - BulletMoveForward
    Changes made to the BulletMoveForward Script

--- Assets/Scripts/BulletMoveForward.cs
+++ Assets/Scripts/BulletMoveForward.cs
@@ -3,25 +3,20 @@ using System.Collections;
 
 public class BulletMoveForward : MonoBehaviour {
 
-    private float acceleration = 50f;
+    private float acceleration = 50f; // The acceleration of the bullet
+    private float initialVelocity = 5f; // The speed of the bullet at first
 
-    private float initialVelocity = 5f;
+    private Rigidbody2D bulletRB; // Declaring variable to rigid body component, ie the bullet
 
-    private Rigidbody2D ourRigidbody;
-
-    // Use this for initialization
-    void Start()
+    void Start() // Method called once the game has started
     {
-        ourRigidbody = GetComponent<Rigidbody2D>();
-
-        ourRigidbody.velocity = Vector2.up * initialVelocity;
+        bulletRB = GetComponent<Rigidbody2D>();
+        bulletRB.velocity = Vector2.up * initialVelocity;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() // Method called every frame, keeps the speed of the bullet constant 
     {
         Vector2 ForceToAdd = Vector2.up * acceleration * Time.deltaTime;
-
-        ourRigidbody.AddForce(ForceToAdd);
+        bulletRB.AddForce(ForceToAdd);
     }
 }
