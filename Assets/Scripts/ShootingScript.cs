108327a414941ecd38054be553fded67cb9cc11f
EdAdams12 <E_A055@student.usc.edu.au>
2024-03-30 16:07:43 +1000

    Updates to 2 Scripts
    ShootingScript:
    - Comments and layout of the script were changed
    
    SpawnOverTimeScript
    - Commens and layout again were changed

--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -4,52 +4,35 @@ using System.Collections;
 public class ShootingScript : MonoBehaviour
 {
 
-    [SerializeField]
-    private GameObject bullet;
-
+    [SerializeField] private GameObject bullet;
     private float lastFiredTime = 0f;
-
-    [SerializeField]
-    private float fireDelay = 1f;
-
+    [SerializeField] private float fireDelay = 1f;
     private float bulletOffset = 2f;
 
-    void Start()
+    void Start() // Do some math to perfectly spawn bullets in front of us
     {
-        // Do some math to perfectly spawn bullets in front of us
         bulletOffset = GetComponent<Renderer>().bounds.size.y / 2 // Half of our size
             + bullet.GetComponent<Renderer>().bounds.size.y / 2; // Plus half of the bullet size
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void Update() // Update is called once per frame
     {
         if (Input.GetButton("Fire1"))
         {
             float CurrentTime = Time.time;
 
-            // Have a delay so we don't shoot too many bullets
-            if (CurrentTime - lastFiredTime > fireDelay)
+            if (CurrentTime - lastFiredTime > fireDelay) // Have a delay so we don't shoot too many bullets
             {
                 Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y + bulletOffset);
-
                 Instantiate(bullet, spawnPosition, transform.rotation);
-
                 lastFiredTime = CurrentTime;
             }
-
-            //print("Shoot!");
         }
     }
 
-    /// <summary>
-    /// SampleMethod is a sample of how to use abstraction by
-    /// specification. It converts a provided integer to a float.
-    /// </summary>
-    /// <param name="number">any integer</param>
-    /// <returns>the number parameter as a float</returns>
-    public float SampleMethod(int number) {
+    public float SampleMethod(int number) // Sample of how to use abstraction by specification. Converting int to float
+    {
         return number;
     }
-
 }
--- Assets/Scripts/SpawnOverTimeScript.cs
+++ Assets/Scripts/SpawnOverTimeScript.cs
@@ -4,28 +4,16 @@ using System.Collections;
 public class SpawnOverTimeScript : MonoBehaviour
 {
 
-    // Object to spawn
-    [SerializeField]
-    private GameObject spawnObject;
-
-    // Delay between spawns
-    [SerializeField]
-    private float spawnDelay = 2f;
+    [SerializeField] private GameObject spawnObject; // Object to spawn
+    [SerializeField] private float spawnDelay = 2f; // Delay between spawns
 
     private Renderer ourRenderer;
-
-    // Use this for initialization
-    void Start()
+    
+    void Start() // Use this for initialization
     {
-
         ourRenderer = GetComponent<Renderer>();
-
-        // Stop our Spawner from being visible!
-        ourRenderer.enabled = false;
-
-        // Call the given function after spawnDelay seconds, 
-        // and then repeatedly call it after spawnDelay seconds.
-        InvokeRepeating("Spawn", spawnDelay, spawnDelay);
+        ourRenderer.enabled = false; // Stop our Spawner from being visible!
+        InvokeRepeating("Spawn", spawnDelay, spawnDelay); // Calls said function after delay, then repeats forever
     }
 
     void Spawn()
@@ -33,10 +21,7 @@ public class SpawnOverTimeScript : MonoBehaviour
         float x1 = transform.position.x - ourRenderer.bounds.size.x / 2;
         float x2 = transform.position.x + ourRenderer.bounds.size.x / 2;
 
-        // Randomly pick a point within the spawn object
-        Vector2 spawnPoint = new Vector2(Random.Range(x1, x2), transform.position.y);
-
-        // Spawn the object at the 'spawnPoint' position
-        Instantiate(spawnObject, spawnPoint, Quaternion.identity);
+        Vector2 spawnPoint = new Vector2(Random.Range(x1, x2), transform.position.y); // Randomly pick a point within the spawn object
+        Instantiate(spawnObject, spawnPoint, Quaternion.identity); // Spawn the object at the 'spawnPoint' position
     }
 }
