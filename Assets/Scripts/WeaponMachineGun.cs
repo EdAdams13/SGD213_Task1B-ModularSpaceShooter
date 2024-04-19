f7823cc27900edb8ebca46aff9b3bf7847bbc1ce
EdAdams12 <E_A055@student.usc.edu.au>
2024-04-19 19:43:11 +1000

    Fixing the change in weapon upon pickup
    So during this time i want to say I learnt more about what was happening with the code between different scripts. I learnt what was needed inside the pickup and the player. At the end of this the player starts with the 'machine gun' and changes to the triple shot when colliding with the pickup

--- Assets/Scripts/WeaponMachineGun.cs
+++ Assets/Scripts/WeaponMachineGun.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponMachineGun : WeaponBase {
-
+public class WeaponMachineGun : WeaponBase 
+{
     /// <summary>
     /// Shoot will spawn a new bullet, provided enough time has passed compared to our fireDelay.
     /// </summary>
-    public override void Shoot() {
-        // get the current time
-        float currentTime = Time.time;
 
-        // if enough time has passed since our last shot compared to our fireDelay, spawn our bullet
-        if (currentTime - lastFiredTime > fireDelay) {
-            // create our bullet
-            GameObject newBullet = Instantiate(bullet, bulletSpawnPoint.position, transform.rotation);
-            // update our shooting state
-            lastFiredTime = currentTime;
+    public override void Shoot() 
+    {
+        
+        float currentTime = Time.time; // get the current time
+        
+        
+        if (currentTime - lastFiredTime > fireDelay) // if enough time has passed since our last shot compared to our fireDelay, spawn our bullet
+        {
+            GameObject newBullet = Instantiate(bullet, bulletSpawnPoint.position, transform.rotation); // create our bullet
+        
+            lastFiredTime = currentTime;  // update our shooting state
+            
+            print("Shoot Machine Gun");
         }
     }
 }
--- Assets/Scripts/WeaponTripleShot.cs
+++ Assets/Scripts/WeaponTripleShot.cs
@@ -7,24 +7,27 @@ public class WeaponTripleShot : WeaponBase {
     /// <summary>
     /// Shoot will spawn a three bullets, provided enough time has passed compared to our fireDelay.
     /// </summary>
-    public override void Shoot() {
-        // get the current time
-        float currentTime = Time.time;
 
-        print("Shoot triple shot");
-        // if enough time has passed since our last shot compared to our fireDelay, spawn our bullet
-        if (currentTime - lastFiredTime > fireDelay) {
+    public override void Shoot() 
+    {
+        
+        float currentTime = Time.time; // get the current time
+          // if enough time has passed since our last shot compared to our fireDelay, spawn our bullet
+      
+        if (currentTime - lastFiredTime > fireDelay) 
+        {
             float x = -0.5f;
-            // create 3 bullets
-            for (int i = 0; i < 3; i++) {
-                // create our bullet
-                GameObject newBullet = Instantiate(bullet, bulletSpawnPoint.position, transform.rotation);
-                // set their direction
-                newBullet.GetComponent<MoveConstantly>().Direction = new Vector2(x + 0.5f * i, 0.5f);
-            }
+            
+            for (int i = 0; i < 3; i++) // create 3 bullets 
+            {
+                GameObject newBullet = Instantiate(bullet, bulletSpawnPoint.position, transform.rotation); // create our bullet
 
-            // update our shooting state
-            lastFiredTime = currentTime;
+                newBullet.GetComponent<MoveConstantly>().Direction = new Vector2(x + 0.5f * i, 0.5f); // set their direction
+            }
+            
+            print("Shoot triple shot");
+
+            lastFiredTime = currentTime;  // update our shooting state
         }
     }
 }
